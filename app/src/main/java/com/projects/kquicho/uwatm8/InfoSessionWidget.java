c7cbec0856c22815697dd3421d705c1bc4200a04
kevinthepanda <flipped_kev@hotmail.com>
2016-03-04 22:48:28 -0500

    Added info sessions and reordering of recyclerview

--- app/src/main/java/com/projects/kquicho/uwatm8/InfoSessionWidget.java
+++ app/src/main/java/com/projects/kquicho/uwatm8/InfoSessionWidget.java
@@ -1,21 +1,46 @@
 package com.projects.kquicho.uwatm8;
 
+
+import com.projects.kquicho.uw_api_client.Core.APIResult;
 import com.projects.kquicho.uw_api_client.Core.JSONDownloader;
+import com.projects.kquicho.uw_api_client.Core.UWOpenDataAPI;
 import com.projects.kquicho.uw_api_client.Resources.ResourcesParser;
 
-public class InfoSessionWidget extends HomeWidget implements JSONDownloader.onDownloadListener {
+public class InfoSessionWidget implements JSONDownloader.onDownloadListener {
 
-    public static HomeWidget getInstance(UWClientResponseHandler handler) {
+    private final String LOGCAT_TAG = "InfoSessionWidget";
+    private static InfoSessionWidget mInstance = null;
+    private static ResourcesParser mParser;
+    private UWClientResponseHandler mHandler;
+
+    public static InfoSessionWidget getInstance(UWClientResponseHandler handler) {
         if(mInstance == null){
-            mParser = new ResourcesParser();
-            mParser.setParseType(ResourcesParser.ParseType.INFOSESSIONS.ordinal());
             mInstance = new InfoSessionWidget(handler);
         }
         return mInstance;
     }
 
     private InfoSessionWidget(UWClientResponseHandler handler) {
-        super(handler);
+        mParser = new ResourcesParser();
+        mParser.setParseType(ResourcesParser.ParseType.INFOSESSIONS.ordinal());
+        mHandler = handler;
+        String url = UWOpenDataAPI.buildURL(mParser.getEndPoint());
+        JSONDownloader downloader = new JSONDownloader(url);
+        downloader.setOnDownloadListener(this);
+        downloader.start();
     }
+
+    @Override
+    public void onDownloadFail(String givenURL, int index) {
+        mHandler.onError(LOGCAT_TAG + ": " + "Download failed.. url = " + givenURL );
+    }
+
+    @Override
+    public void onDownloadComplete(APIResult apiResult) {
+        mParser.setAPIResult(apiResult);
+        mParser.parseJSON();
+        mHandler.onSuccess(mParser);
+    }
+
 
 }
