68d76a79b7b83f6f3c77624d1077059d6825a384
kevinthepanda <flipped_kev@hotmail.com>
2016-03-10 18:30:14 -0500

    Fix database model and operations

--- app/src/main/java/com/projects/kquicho/uwatm8/InfoSessionAlarmReceiver.java
+++ app/src/main/java/com/projects/kquicho/uwatm8/InfoSessionAlarmReceiver.java
@@ -5,43 +5,16 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
-import com.projects.kquicho.uw_api_client.Resources.InfoSession;
-
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
-
-
 public class InfoSessionAlarmReceiver extends BroadcastReceiver {
     public final static String TAG = "InfoSessionAlarmReceive";
-    public final static String INFO_SESSION = "infoSession";
+    public final static String INFO_SESSION_MODEL = "infoSessionModel";
 
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.d(TAG, "onReceive");
-        InfoSession infoSession = intent.getParcelableExtra(INFO_SESSION);
-
-        DateFormat format = new SimpleDateFormat("yyyy-MM-dd", Locale.CANADA);
-        Date date = null;
-        try {
-            date = format.parse(infoSession.getDate());
-        } catch (ParseException exception) {
-            Log.e(TAG, "onReceive ParseException: " + exception.getMessage());
-        }
-        if (date == null) {
-            return;
-        }
-        InfoSessionDBHelper.getInstance(context)
-                .addInfoSession(new InfoSessionDBModel(String.valueOf(infoSession.getId()), date.getTime()));
-
-
+        InfoSessionDBModel infoSessionDBModel = intent.getParcelableExtra(INFO_SESSION_MODEL);
         Intent intentService = new Intent(context, InfoSessionService.class);
-        intentService.putExtra(InfoSessionService.ID, infoSession.getId());
-        intentService.putExtra(InfoSessionService.EMPLOYER, infoSession.getEmployer());
-        intentService.putExtra(InfoSessionService.TIME, infoSession.getStart_time() + "-" + infoSession.getEnd_time());
-        intentService.putExtra(InfoSessionService.BUILDING, infoSession.getBuildingCode());
+        intentService.putExtra(InfoSessionService.INFO_SESSION_MODEL, infoSessionDBModel);
         context.startService(intentService);
     }
 
