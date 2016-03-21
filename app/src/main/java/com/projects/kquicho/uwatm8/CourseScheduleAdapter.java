0a5c8c418a3d1932c4c45c62f8f9eb29646a304d
kevinthepanda <flipped_kev@hotmail.com>
2016-03-21 00:40:41 -0400

    Added new schedule rows that are expandable

---
+++ app/src/main/java/com/projects/kquicho/uwatm8/CourseScheduleAdapter.java
@@ -0,0 +1,339 @@
+package com.projects.kquicho.uwatm8;
+
+import android.animation.ArgbEvaluator;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.text.Html;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.h6ah4i.android.widget.advrecyclerview.expandable.ExpandableItemConstants;
+import com.h6ah4i.android.widget.advrecyclerview.utils.AbstractExpandableItemAdapter;
+import com.h6ah4i.android.widget.advrecyclerview.utils.AbstractExpandableItemViewHolder;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ * Created by Kevin Quicho on 3/19/2016.
+ */
+public class CourseScheduleAdapter
+        extends AbstractExpandableItemAdapter<CourseScheduleAdapter.MyGroupViewHolder, CourseScheduleAdapter.MyBaseViewHolder>{
+    public static final String TAG = "CourseScheduleAdapter";
+    private final int CLASS = 0, ENROLLMENT = 1;
+
+    private CourseScheduleData mData;
+
+    Drawable mEnrollmentStatusOpen;
+    Drawable mEnrollmentStatusFull;
+
+    // NOTE: Make accessible with short name
+    private interface Expandable extends ExpandableItemConstants {
+    }
+
+    public static abstract class MyBaseViewHolder extends AbstractExpandableItemViewHolder {
+        public View mContainer;
+
+        public MyBaseViewHolder(View v) {
+            super(v);
+            mContainer =  v.findViewById(R.id.container);
+        }
+    }
+
+    public static class MyGroupViewHolder extends MyBaseViewHolder {
+        public ExpandableItemIndicator mIndicator;
+        public TextView mSection;
+        public TextView mInstructor;
+        public TextView mTime;
+        public TextView mLocation;
+        public TextView mExpandMessage;
+        public ImageView mEnrollmentStatus;
+
+        public MyGroupViewHolder(View v) {
+            super(v);
+            mIndicator = (ExpandableItemIndicator) v.findViewById(R.id.indicator);
+            mSection = (TextView) v.findViewById(R.id.section);
+            mInstructor= (TextView) v.findViewById(R.id.instructor);
+            mTime = (TextView) v.findViewById(R.id.time);
+            mLocation = (TextView) v.findViewById(R.id.location);
+            mExpandMessage = (TextView) v.findViewById(R.id.expand_message);
+            mEnrollmentStatus = (ImageView) v.findViewById(R.id.enrollment_status);
+
+        }
+    }
+
+    public static class MyChildClassViewHolder extends MyBaseViewHolder {
+        public TextView mTime;
+        public TextView mLocation;
+
+        public MyChildClassViewHolder(View v) {
+            super(v);
+            mTime = (TextView) v.findViewById(R.id.time);
+            mLocation = (TextView) v.findViewById(R.id.location);
+        }
+
+    }
+
+    public static class MyChildEnrollmentViewHolder extends MyBaseViewHolder  {
+        public TextView mEnrollment;
+        public TextView mEnrollmentGroup;
+        public ProgressBar mProgressBar;
+
+        public MyChildEnrollmentViewHolder(View v) {
+            super(v);
+            mEnrollment = (TextView) v.findViewById(R.id.enrollment);
+            mEnrollmentGroup = (TextView) v.findViewById(R.id.enrollment_label);
+            mProgressBar = (ProgressBar) v.findViewById(R.id.progressBar);
+        }
+    }
+
+    public CourseScheduleAdapter(CourseScheduleData data, Drawable enrollmentOpen, Drawable enrollmentFull){
+        mData = data;
+        mEnrollmentStatusOpen = enrollmentOpen;
+        mEnrollmentStatusFull = enrollmentFull;
+
+        setHasStableIds(true);
+    }
+
+    @Override
+    public int getGroupCount() {
+        return mData.getGroupCount();
+    }
+
+    @Override
+    public int getChildCount(int i) {
+        return  mData.getChildCount(i);
+    }
+
+    @Override
+    public long getGroupId(int i) {
+        return mData.getGroupItem(i).getGroupId();
+    }
+
+    @Override
+    public long getChildId(int i, int j) {
+        return mData.getChildItem(i,j).getChildId();
+    }
+
+    @Override
+    public int getGroupItemViewType(int i) {
+        return 0;
+    }
+
+    @Override
+    public int getChildItemViewType(int i, int j) {
+        if(mData.getChildItem(i,j) instanceof  CourseSectionClassData){
+            return CLASS;
+        }else{
+            return ENROLLMENT;
+        }
+    }
+
+    @Override
+    public MyGroupViewHolder onCreateGroupViewHolder(ViewGroup viewGroup, int i) {
+        final LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
+        final View v = inflater.inflate(R.layout.section_row, viewGroup, false);
+        return new MyGroupViewHolder(v);
+    }
+
+    @Override
+    public MyBaseViewHolder onCreateChildViewHolder(ViewGroup viewGroup, int i) {
+        MyBaseViewHolder viewHolder;
+        LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
+        View view;
+        switch (i) {
+            case CLASS:
+                view = inflater.inflate(R.layout.class_row, viewGroup, false);
+                viewHolder = new MyChildClassViewHolder(view);
+                break;
+            case ENROLLMENT:
+                view = inflater.inflate(R.layout.section_details_row, viewGroup, false);
+                viewHolder = new MyChildEnrollmentViewHolder(view);
+                break;
+            default:
+                view = inflater.inflate(R.layout.class_row, viewGroup, false);
+                viewHolder = new MyChildClassViewHolder(view);
+                break;
+        }
+        return viewHolder;
+}
+
+    @Override
+    public void onBindGroupViewHolder(MyGroupViewHolder myGroupViewHolder, int i, int j) {
+        final CourseSectionData courseSectionData = (CourseSectionData)mData.getGroupItem(i);
+        myGroupViewHolder.mSection.setText(courseSectionData.getSection());
+        myGroupViewHolder.mInstructor.setText(courseSectionData.getInstructor());
+
+        DateFormat dateFormat = new SimpleDateFormat("HH:mm", Locale.CANADA) ;
+        DateFormat newFormat = new SimpleDateFormat("h:mm a", Locale.CANADA);
+        DateFormat newFormatWithoutSuffix = new SimpleDateFormat("h:mm", Locale.CANADA);
+        String time = "";
+        try{
+            Date startTime = dateFormat.parse(courseSectionData.getStartTime());
+            Date endTime = dateFormat.parse(courseSectionData.getEndTime());
+            String sStartTime = newFormat.format(startTime);
+            String sEndTime = newFormat.format(endTime);
+
+            if(sStartTime.charAt(sStartTime.length() -2) == sEndTime.charAt(sEndTime.length() -2)){
+                time = newFormatWithoutSuffix.format(startTime) + " - " + sEndTime;
+            }else{
+                time = sStartTime + " - " + sEndTime;
+            }
+
+        }catch (ParseException ex){
+            Log.e(TAG, "createClass ParseException: " + ex.getMessage());
+        }
+
+        String finalTimeText = "TBA";
+        if(!courseSectionData.getWeekdays().equals("null")){
+            finalTimeText = time + " " + courseSectionData.getWeekdays();
+        }
+
+        String location = "TBA - ";
+        if(!courseSectionData.getBuilding().equals("null") && !courseSectionData.getRoom().equals("null")){
+            location = courseSectionData.getBuilding() + " " + courseSectionData.getRoom()  + " - ";
+        }else if(courseSectionData.getCampus().equals("ONLN ONLINE")){
+            location = "";
+            finalTimeText = "N/A";
+        }
+        myGroupViewHolder.mTime.setText(finalTimeText);
+        myGroupViewHolder.mLocation.setText(location + courseSectionData.getCampus());
+
+        int visibility = mData.getChildCount(i) == 1 ? View.GONE : View.VISIBLE;
+        myGroupViewHolder.mExpandMessage.setVisibility(visibility);
+
+        Drawable status = courseSectionData.getEnrollmentTotal() < courseSectionData.getEnrollmentCapacity() ?
+                mEnrollmentStatusOpen : mEnrollmentStatusFull;
+        myGroupViewHolder.mEnrollmentStatus.setImageDrawable(status);
+
+        // mark as clickable
+        myGroupViewHolder.itemView.setClickable(true);
+
+        // set background resource (target view ID: container)
+        final int expandState = myGroupViewHolder.getExpandStateFlags();
+
+        if ((expandState & ExpandableItemConstants.STATE_FLAG_IS_UPDATED) != 0) {
+            boolean isExpanded;
+            boolean animateIndicator = ((expandState & Expandable.STATE_FLAG_HAS_EXPANDED_STATE_CHANGED) != 0);
+
+            if ((expandState & Expandable.STATE_FLAG_IS_EXPANDED) != 0) {
+                myGroupViewHolder.mExpandMessage.setVisibility(View.GONE);
+                isExpanded = true;
+            } else {
+                isExpanded = false;
+                for(int k = mData.getChildCount(i) -1; k >= 0; k-- ){
+                    AbstractExpandableData.ChildData data = mData.getChildItem(i,k);
+                    if(data instanceof CourseEnrollmentData){
+                        ((CourseEnrollmentData)data).setHasAnimationRan(false);
+                    }else{
+                        break;
+                    }
+                }
+            }
+
+            myGroupViewHolder.mIndicator.setExpandedState(isExpanded, animateIndicator);
+        }
+
+    }
+
+    @Override
+    public void onBindChildViewHolder(MyBaseViewHolder myBaseViewHolder, int groupPosition, int childPosition, int viewType) {
+        switch (viewType){
+            case CLASS:
+                CourseSectionClassData classData = (CourseSectionClassData)mData.getChildItem(groupPosition,childPosition);
+                MyChildClassViewHolder classHolder = (MyChildClassViewHolder)myBaseViewHolder;
+                DateFormat dateFormat = new SimpleDateFormat("HH:mm", Locale.CANADA) ;
+                DateFormat newFormat = new SimpleDateFormat("h:mm a", Locale.CANADA);
+                DateFormat newFormatWithoutSuffix = new SimpleDateFormat("h:mm", Locale.CANADA);
+                String time = "";
+                try{
+                    Date startTime = dateFormat.parse(classData.getStartTime());
+                    Date endTime = dateFormat.parse(classData.getEndTime());
+                    String sStartTime = newFormat.format(startTime);
+                    String sEndTime = newFormat.format(endTime);
+
+                    if(sStartTime.charAt(sStartTime.length() -2) == sEndTime.charAt(sEndTime.length() -2)){
+                        time = newFormatWithoutSuffix.format(startTime) + " - " + sEndTime;
+                    }else{
+                        time = sStartTime + " - " + sEndTime;
+                    }
+
+                }catch (ParseException ex){
+                    Log.e(TAG, "createClass ParseException: " + ex.getMessage());
+                }
+                String finalTimeText = "TBA";
+                if(!classData.getWeekdays().equals("null")){
+                    finalTimeText = time + " " + classData.getWeekdays();
+                }
+                classHolder.mTime.setText(finalTimeText);
+
+                String location = "TBA";
+                if(!classData.getBuilding().equals("null") && !classData.getRoom().equals("null")){
+                    location = classData.getBuilding() + " " + classData.getRoom() ;
+                }
+                classHolder.mLocation.setText(location + " - " + classData.getCampus());
+
+                break;
+            case ENROLLMENT:
+                CourseEnrollmentData enrollmentData = (CourseEnrollmentData)mData.getChildItem(groupPosition,childPosition);
+                final MyChildEnrollmentViewHolder enrollmentHolder = (MyChildEnrollmentViewHolder)myBaseViewHolder;
+
+                String enrollmentGroup = enrollmentData.getGroup();
+                if(enrollmentGroup.equals("")){
+                    enrollmentHolder.mEnrollmentGroup.setText("Enrollment");
+                }else{
+                    enrollmentHolder.mEnrollmentGroup.setText("Reserves - " + enrollmentGroup);
+                }
+
+                final ProgressBar progressBar = enrollmentHolder.mProgressBar;
+                progressBar.setMax(enrollmentData.getEnrollmentCapacity());
+                final String enrollmentCapacity = String.valueOf(enrollmentData.getEnrollmentCapacity());
+
+                if(enrollmentData.hasAnimationRan()){
+                    enrollmentHolder.mEnrollment.setText(enrollmentData.getEnrollmentTotal() + "/" + enrollmentCapacity);
+                    progressBar.setProgress(enrollmentData.getEnrollmentTotal());
+                }else {
+                    enrollmentHolder.mEnrollment.setText("0/" + enrollmentCapacity);
+                    progressBar.setProgress(0);
+                    ObjectAnimator animation = ObjectAnimator.ofInt(progressBar, "progress", 0, enrollmentData.getEnrollmentTotal()); // see this max value coming back here, we animale towards that value
+                    animation.setDuration(500); //in milliseconds
+                    animation.setInterpolator(new DecelerateInterpolator());
+                    animation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                        @Override
+                        public void onAnimationUpdate(ValueAnimator animation) {
+                            Integer current = (Integer) animation.getAnimatedValue();
+                            enrollmentHolder.mEnrollment.setText(current.toString() + "/" + enrollmentCapacity);
+                        }
+                    });
+                    animation.setStartDelay(300);
+                    animation.start();
+                    enrollmentData.setHasAnimationRan(true);
+                }
+
+        }
+    }
+
+    @Override
+    public boolean onCheckCanExpandOrCollapseGroup(MyGroupViewHolder myGroupViewHolder, int i, int i1, int i2, boolean b) {
+        if (!(myGroupViewHolder.itemView.isEnabled() && myGroupViewHolder.itemView.isClickable())) {
+            return false;
+        }
+
+        return true;
+    }
+
+
+
+}
---
+++ app/src/main/java/com/projects/kquicho/uwatm8/CourseSectionClassData.java
@@ -0,0 +1,105 @@
+package com.projects.kquicho.uwatm8;
+
+/**
+ * Created by Kevin Quicho on 3/19/2016.
+ */
+public class CourseSectionClassData extends AbstractExpandableData.ChildData {
+    private String mStartTime = null;
+    private String mEndTime = null;
+    private String mWeekdays = null;
+    private String mBuilding = null;
+    private String mRoom = null;
+    private String mCampus = null;
+    private String mDate = null;
+
+    private CourseSectionClassData(long id, String startTime, String endTime, String weekdays, String building, String room, String campus, String date) {
+        super(id);
+        mStartTime = startTime;
+        mEndTime = endTime;
+        mWeekdays = weekdays;
+        mBuilding = building;
+        mRoom = room;
+        mCampus = campus;
+        mDate = date;
+    }
+
+    public String getStartTime() {
+        return mStartTime;
+    }
+
+    public String getEndTime() {
+        return mEndTime;
+    }
+
+    public String getWeekdays() {
+        return mWeekdays;
+    }
+
+    public String getBuilding() {
+        return mBuilding;
+    }
+
+    public String getRoom() {
+        return mRoom;
+    }
+
+    public String getCampus() {
+        return mCampus;
+    }
+
+    public static class Builder{
+        private String mStartTime = null;
+        private String mEndTime = null;
+        private String mWeekdays = null;
+        private String mBuilding = null;
+        private String mRoom = null;
+        private String mCampus = null;
+        private String mDate = null;
+        private long mId;
+
+        public Builder(long id){
+            mId = id;
+        }
+
+        public Builder startTime(String startTime){
+            mStartTime = startTime;
+            return this;
+        }
+
+        public Builder endTime(String endTime){
+            mEndTime = endTime;
+            return this;
+        }
+
+        public Builder weekdays(String weekdays){
+            mWeekdays = weekdays;
+            return this;
+        }
+
+        public Builder building(String building){
+            mBuilding = building;
+            return this;
+        }
+
+        public Builder room(String room){
+            mRoom = room;
+            return this;
+        }
+
+        public Builder campus(String campus){
+            mCampus = campus;
+            return this;
+        }
+
+        public Builder date(String date){
+            mDate = date;
+            return this;
+        }
+
+
+        public CourseSectionClassData createCourseSectionClassData(){
+            return new CourseSectionClassData(mId, mStartTime, mEndTime, mWeekdays, mBuilding, mRoom, mCampus, mDate);
+        }
+
+    }
+}
