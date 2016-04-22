059e5462aacf828dc2724eeeb49ff29708fa7e2a
kevinthepanda <flipped_kev@hotmail.com>
2016-04-22 04:44:27 -0400

    Handle on config changes for HomeFragment

--- app/src/main/java/com/projects/kquicho/uwatm8/UWData.java
+++ app/src/main/java/com/projects/kquicho/uwatm8/UWData.java
@@ -1,19 +1,23 @@
 package com.projects.kquicho.uwatm8;
 
 
-import com.projects.kquicho.uw_api_client.Core.UWParser;
+import android.os.Parcel;
+import android.os.Parcelable;
 
-public class UWData {
-    private UWParser mParser = null;
+public class UWData implements Parcelable{
     private String mWidgetTag;
     private boolean mPinned = false;
+    private boolean mLoading = true;
 
-    public UWData(UWParser parser, String widgetTag){
-        mParser = parser;
+    protected UWData(){}
+    public UWData(String widgetTag){
         mWidgetTag = widgetTag;
     }
-    public UWParser getParser(){
-        return mParser;
+
+    public UWData(Parcel in){
+        mWidgetTag = in.readString();
+        mPinned = in.readByte() != 0;
+        mLoading = in.readByte() != 0;
     }
 
     public boolean isPinned(){
@@ -27,4 +31,34 @@ public class UWData {
     public String getWidgetTag(){
         return mWidgetTag;
     }
+
+    public boolean isLoading(){
+        return mLoading;
+    }
+    protected void finishedLoading(){
+        mLoading = false;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(mWidgetTag);
+        dest.writeByte((byte) (mPinned ? 1 : 0));
+        dest.writeByte((byte) (mLoading ? 1 : 0));
+    }
+
+    public static final Parcelable.Creator CREATOR = new Parcelable.Creator() {
+        public UWData createFromParcel(Parcel in) {
+            return new UWData(in);
+        }
+
+        public UWData[] newArray(int size) {
+            return new UWData[size];
+        }
+    };
+
 }
